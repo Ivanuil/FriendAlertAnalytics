d019a38416a46b5d0d309562ff31c0528c521c55
Ivanuil <ivo-pronin@yandex.ru>
2024-07-04 15:08:06 +0300

    Added demo endpoint, returning an image

--- src/web/controller/demo_controller.py
+++ src/web/controller/demo_controller.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.responses import Response
 
 app = FastAPI()
 saved_text = "No data"
@@ -20,3 +22,25 @@ async def save(text: str):
 async def retrieve():
     global saved_text
     return saved_text
+
+
+@app.get("/image")
+async def image():
+    return FileResponse("misc/test-data/dice.png")
+
+
+# Notice: For some reason Pycharm lights responses and response_class in the following annotation red, as if it was
+# an error. But this code works LOL
+@app.get("/imageBytes",
+         responses={
+             200: {
+                 "content": {"image/png": {}},
+             }
+         },
+         response_class=Response
+         )
+async def image_bytes():
+    in_file = open("misc/test-data/dice.png", "rb")
+    data = in_file.read()
+    in_file.close()
+    return Response(content=data, media_type="image/png")
